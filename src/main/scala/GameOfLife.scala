5d0763dcbdaab2fa97d58cf2bfb4e8652b47a209
kabutopia <eyssartk@gmail.com>
2018-12-21 20:05:53 +0100

    Working without display

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,34 +1,57 @@
-object GameOfLife {
+package GameOfLife
 
-  class Board(cells: List[Cell]) {
+object GoL {
+  case class Board(val cells: Set[Cell])
+  case class Cell(val x: Int, val y: Int)
 
-    def append(c: Cell) = {
-      this.cells :+ c
+  var board = Board(Set())
+
+  def apply(l: List[(Int,Int)]) = {
+    board = Board((for (c <- l) yield Cell(c._1, c._2)).toSet)
+  }
+  
+  def update () = {
+    board = getAlive(countOccur(getNeighbours(board)), board)
+  }
+  
+  def display (b: Board) = ???
+
+  def neighbours (c: Cell) : List[Cell] = c match {
+    case Cell(x,y) => List(
+        Cell(x-1,y-1),
+        Cell(x-1,y  ),
+        Cell(x-1,y+1),
+        Cell(x  ,y-1),
+        Cell(x  ,y+1),
+        Cell(x+1,y-1),
+        Cell(x+1,y  ),
+        Cell(x+1,y+1))
     }
 
-    def update: Board = {
-      val bNext: Board = new Board(Nil)
-      val mTemp = Map[(Int,Int),Int]()
-      for (c <- this.cells) {
-        print(mTemp.get((c.x,c.y)))
-      }
-      return bNext
-    }
+  def getNeighbours(b: Board) : List[Cell] =
+    b.cells.toList.map(neighbours(_)).flatten
 
-    def display = ???
+  def countOccur (l: List[Cell]) : Map[Cell,Int] =
+    l.groupBy(identity).mapValues(_.size)
+  
 
-
+  def isAlive(c: Cell, n: Int, prev: Board) : Boolean = {
+    if (n==3) true
+    else if (n==2 && prev.cells.contains(c)) true
+    else false
   }
 
-  class Cell(x: Int, y: Int) {
-
-  }
-
+  def getAlive (l: Map[Cell,Int], prev: Board) : Board =
+    Board( l.filter(x => isAlive(x._1,x._2,prev)).keySet)
 }
 
-object Main extends App {
-  println("Hello")
 
+
+object Main extends App {
+  GoL(List((-1,0),(0,0),(1,0)))
+  println(GoL.board)
+  GoL.update()
+  println(GoL.board)
 }
 
 
