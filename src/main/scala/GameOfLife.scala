4b02805efb72004963ad422672f7080a6b8e4c49
kabutopia <eyssartk@gmail.com>
2018-12-21 21:14:00 +0100

    Ugly display

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -6,15 +6,24 @@ object GoL {
 
   var board = Board(Set())
 
-  def apply(l: List[(Int,Int)]) = {
+  def set(l: List[(Int,Int)]) = {
     board = Board((for (c <- l) yield Cell(c._1, c._2)).toSet)
   }
   
-  def update () = {
+  def update() = {
     board = getAlive(countOccur(getNeighbours(board)), board)
   }
   
-  def display (b: Board) = ???
+  def display() = {
+    val xmin = board.cells.minBy(_.x).x -2
+    val xmax = board.cells.maxBy(_.x).x +2
+    val ymin = board.cells.minBy(_.y).y -2
+    val ymax = board.cells.maxBy(_.y).y +2
+
+    var myMatrix = Array.fill(xmax-xmin+1,ymax-ymin+1)('.')
+    for (c <- board.cells) myMatrix(c.x-xmin)(c.y-ymin) = '#'
+    myMatrix foreach { row => row foreach print; println }
+  }
 
   def neighbours (c: Cell) : List[Cell] = c match {
     case Cell(x,y) => List(
@@ -45,14 +54,17 @@ object GoL {
     Board( l.filter(x => isAlive(x._1,x._2,prev)).keySet)
 }
 
-
-
 object Main extends App {
-  GoL(List((-1,0),(0,0),(1,0)))
-  println(GoL.board)
-  GoL.update()
-  println(GoL.board)
+
+  val toad = List((-1,0),(0,0),(1,0),(-2,-1),(-1,-1),(0,-1))
+  val blinker = List((-1,0),(0,0),(1,0))
+  val blinkerOff = List((-10,20),(-11,20),(-12,20))
+  val glider = List((-2,0),(-1,0),(0,0),(0,1),(-1,2))
+
+  GoL.set(glider)
+  for (i <- 1 to 10) {
+    GoL.display()
+    GoL.update()
+  }
 }
-
-
 
